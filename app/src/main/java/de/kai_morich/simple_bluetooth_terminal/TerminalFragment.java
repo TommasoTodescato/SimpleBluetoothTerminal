0a612ee65eecd126e3b7b6b987127542a1ff1c22
Tommaso <tommasotodescato@gmail.com>
2023-02-25 08:43:47 +0100

    fixed tabs

--- app/src/main/java/de/kai_morich/simple_bluetooth_terminal/TerminalFragment.java
+++ app/src/main/java/de/kai_morich/simple_bluetooth_terminal/TerminalFragment.java
@@ -33,225 +33,225 @@ import java.util.ArrayDeque;
 
 public class TerminalFragment extends Fragment implements ServiceConnection, SerialListener {
 
-    private enum Connected { False, Pending, True }
+	private enum Connected { False, Pending, True }
 
-    private String deviceAddress;
-    private SerialService service;
+	private String deviceAddress;
+	private SerialService service;
 
-    private TextView receiveText;
-    private TextView sendText;
-    private TextUtil.HexWatcher hexWatcher;
+	private TextView receiveText;
+	private TextView sendText;
+	private TextUtil.HexWatcher hexWatcher;
 
-    private Connected connected = Connected.False;
-    private boolean initialStart = true;
-    private boolean hexEnabled = false;
-    private boolean pendingNewline = false;
-    private String newline = TextUtil.newline_crlf;
+	private Connected connected = Connected.False;
+	private boolean initialStart = true;
+	private boolean hexEnabled = false;
+	private boolean pendingNewline = false;
+	private String newline = TextUtil.newline_crlf;
 
-    /*
-     * Lifecycle
-     */
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-        setRetainInstance(true);
-        deviceAddress = getArguments().getString("device");
-    }
+	/*
+	 * Lifecycle
+	 */
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+		setRetainInstance(true);
+		deviceAddress = getArguments().getString("device");
+	}
 
-    @Override
-    public void onDestroy() {
-        if (connected != Connected.False)
-            disconnect();
-        getActivity().stopService(new Intent(getActivity(), SerialService.class));
-        super.onDestroy();
-    }
+	@Override
+	public void onDestroy() {
+		if (connected != Connected.False)
+			disconnect();
+		getActivity().stopService(new Intent(getActivity(), SerialService.class));
+		super.onDestroy();
+	}
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        if(service != null)
-            service.attach(this);
-        else
-            getActivity().startService(new Intent(getActivity(), SerialService.class)); // prevents service destroy on unbind from recreated activity caused by orientation change
-    }
+	@Override
+	public void onStart() {
+		super.onStart();
+		if(service != null)
+			service.attach(this);
+		else
+			getActivity().startService(new Intent(getActivity(), SerialService.class)); // prevents service destroy on unbind from recreated activity caused by orientation change
+	}
 
-    @Override
-    public void onStop() {
-        if(service != null && !getActivity().isChangingConfigurations())
-            service.detach();
-        super.onStop();
-    }
+	@Override
+	public void onStop() {
+		if(service != null && !getActivity().isChangingConfigurations())
+			service.detach();
+		super.onStop();
+	}
 
-    @SuppressWarnings("deprecation") // onAttach(context) was added with API 23. onAttach(activity) works for all API versions
-    @Override
-    public void onAttach(@NonNull Activity activity) {
-        super.onAttach(activity);
-        getActivity().bindService(new Intent(getActivity(), SerialService.class), this, Context.BIND_AUTO_CREATE);
-    }
+	@SuppressWarnings("deprecation") // onAttach(context) was added with API 23. onAttach(activity) works for all API versions
+	@Override
+	public void onAttach(@NonNull Activity activity) {
+		super.onAttach(activity);
+		getActivity().bindService(new Intent(getActivity(), SerialService.class), this, Context.BIND_AUTO_CREATE);
+	}
 
-    @Override
-    public void onDetach() {
-        try { getActivity().unbindService(this); } catch(Exception ignored) {}
-        super.onDetach();
-    }
+	@Override
+	public void onDetach() {
+		try { getActivity().unbindService(this); } catch(Exception ignored) {}
+		super.onDetach();
+	}
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        if(initialStart && service != null) {
-            initialStart = false;
-            getActivity().runOnUiThread(this::connect);
-        }
-    }
+	@Override
+	public void onResume() {
+		super.onResume();
+		if(initialStart && service != null) {
+			initialStart = false;
+			getActivity().runOnUiThread(this::connect);
+		}
+	}
 
-    @Override
-    public void onServiceConnected(ComponentName name, IBinder binder) {
-        service = ((SerialService.SerialBinder) binder).getService();
-        service.attach(this);
-        if(initialStart && isResumed()) {
-            initialStart = false;
-            getActivity().runOnUiThread(this::connect);
-        }
-    }
+	@Override
+	public void onServiceConnected(ComponentName name, IBinder binder) {
+		service = ((SerialService.SerialBinder) binder).getService();
+		service.attach(this);
+		if(initialStart && isResumed()) {
+			initialStart = false;
+			getActivity().runOnUiThread(this::connect);
+		}
+	}
 
-    @Override
-    public void onServiceDisconnected(ComponentName name) {
-        service = null;
-    }
+	@Override
+	public void onServiceDisconnected(ComponentName name) {
+		service = null;
+	}
 
-    /*
-     * UI
-     */
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_terminal, container, false);
-        receiveText = view.findViewById(R.id.receive_text);                          // TextView performance decreases with number of spans
-        receiveText.setTextColor(getResources().getColor(R.color.colorRecieveText)); // set as default color to reduce number of spans
-        receiveText.setMovementMethod(ScrollingMovementMethod.getInstance());
+	/*
+	 * UI
+	 */
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_terminal, container, false);
+		receiveText = view.findViewById(R.id.receive_text);                          // TextView performance decreases with number of spans
+		receiveText.setTextColor(getResources().getColor(R.color.colorRecieveText)); // set as default color to reduce number of spans
+		receiveText.setMovementMethod(ScrollingMovementMethod.getInstance());
 
-        sendText = view.findViewById(R.id.send_text);
-        hexWatcher = new TextUtil.HexWatcher(sendText);
-        hexWatcher.enable(hexEnabled);
-        sendText.addTextChangedListener(hexWatcher);
-        sendText.setHint(hexEnabled ? "HEX mode" : "");
+		sendText = view.findViewById(R.id.send_text);
+		hexWatcher = new TextUtil.HexWatcher(sendText);
+		hexWatcher.enable(hexEnabled);
+		sendText.addTextChangedListener(hexWatcher);
+		sendText.setHint(hexEnabled ? "HEX mode" : "");
 
-        View sendBtn = view.findViewById(R.id.send_btn);
-        sendBtn.setOnClickListener(v -> send(sendText.getText().toString()));
-        return view;
-    }
+		View sendBtn = view.findViewById(R.id.send_btn);
+		sendBtn.setOnClickListener(v -> send(sendText.getText().toString()));
+		return view;
+	}
 
-    @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.menu_terminal, menu);
-        menu.findItem(R.id.hex).setChecked(hexEnabled);
-    }
+	@Override
+	public void onCreateOptionsMenu(@NonNull Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.menu_terminal, menu);
+		menu.findItem(R.id.hex).setChecked(hexEnabled);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.clear) {
-            receiveText.setText("");
-            return true;
-        } else if (id == R.id.newline) {
-            String[] newlineNames = getResources().getStringArray(R.array.newline_names);
-            String[] newlineValues = getResources().getStringArray(R.array.newline_values);
-            int pos = java.util.Arrays.asList(newlineValues).indexOf(newline);
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setTitle("Newline");
-            builder.setSingleChoiceItems(newlineNames, pos, (dialog, item1) -> {
-                newline = newlineValues[item1];
-                dialog.dismiss();
-            });
-            builder.create().show();
-            return true;
-        } else if (id == R.id.hex) {
-            hexEnabled = !hexEnabled;
-            sendText.setText("");
-            hexWatcher.enable(hexEnabled);
-            sendText.setHint(hexEnabled ? "HEX mode" : "");
-            item.setChecked(hexEnabled);
-            return true;
-        } else {
-            return super.onOptionsItemSelected(item);
-        }
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.clear) {
+			receiveText.setText("");
+			return true;
+		} else if (id == R.id.newline) {
+			String[] newlineNames = getResources().getStringArray(R.array.newline_names);
+			String[] newlineValues = getResources().getStringArray(R.array.newline_values);
+			int pos = java.util.Arrays.asList(newlineValues).indexOf(newline);
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+			builder.setTitle("Newline");
+			builder.setSingleChoiceItems(newlineNames, pos, (dialog, item1) -> {
+				newline = newlineValues[item1];
+				dialog.dismiss();
+			});
+			builder.create().show();
+			return true;
+		} else if (id == R.id.hex) {
+			hexEnabled = !hexEnabled;
+			sendText.setText("");
+			hexWatcher.enable(hexEnabled);
+			sendText.setHint(hexEnabled ? "HEX mode" : "");
+			item.setChecked(hexEnabled);
+			return true;
+		} else {
+			return super.onOptionsItemSelected(item);
+		}
+	}
 
-    /*
-     * Serial + UI
-     */
-    private void connect() {
-        try {
-            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-            BluetoothDevice device = bluetoothAdapter.getRemoteDevice(deviceAddress);
-            status("connecting...");
-            connected = Connected.Pending;
-            SerialSocket socket = new SerialSocket(getActivity().getApplicationContext(), device);
-            service.connect(socket);
-        } catch (Exception e) {
-            onSerialConnectError(e);
-        }
-    }
+	/*
+	 * Serial + UI
+	 */
+	private void connect() {
+		try {
+			BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+			BluetoothDevice device = bluetoothAdapter.getRemoteDevice(deviceAddress);
+			status("connecting...");
+			connected = Connected.Pending;
+			SerialSocket socket = new SerialSocket(getActivity().getApplicationContext(), device);
+			service.connect(socket);
+		} catch (Exception e) {
+			onSerialConnectError(e);
+		}
+	}
 
-    private void disconnect() {
-        connected = Connected.False;
-        service.disconnect();
-    }
+	private void disconnect() {
+		connected = Connected.False;
+		service.disconnect();
+	}
 
-    private void send(String str) {
-        if(connected != Connected.True) {
-            Toast.makeText(getActivity(), "not connected", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        try {
-            String msg;
-            byte[] data;
-            if(hexEnabled) {
-                StringBuilder sb = new StringBuilder();
-                TextUtil.toHexString(sb, TextUtil.fromHexString(str));
-                TextUtil.toHexString(sb, newline.getBytes());
-                msg = sb.toString();
-                data = TextUtil.fromHexString(msg);
-            } else {
-                msg = str;
-                data = (str + newline).getBytes();
-            }
-            SpannableStringBuilder spn = new SpannableStringBuilder(msg + '\n');
-            spn.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorSendText)), 0, spn.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-            receiveText.append(spn);
-            service.write(data);
-        } catch (Exception e) {
-            onSerialIoError(e);
-        }
-    }
+	private void send(String str) {
+		if(connected != Connected.True) {
+			Toast.makeText(getActivity(), "not connected", Toast.LENGTH_SHORT).show();
+			return;
+		}
+		try {
+			String msg;
+			byte[] data;
+			if(hexEnabled) {
+				StringBuilder sb = new StringBuilder();
+				TextUtil.toHexString(sb, TextUtil.fromHexString(str));
+				TextUtil.toHexString(sb, newline.getBytes());
+				msg = sb.toString();
+				data = TextUtil.fromHexString(msg);
+			} else {
+				msg = str;
+				data = (str + newline).getBytes();
+			}
+			SpannableStringBuilder spn = new SpannableStringBuilder(msg + '\n');
+			spn.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorSendText)), 0, spn.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+			receiveText.append(spn);
+			service.write(data);
+		} catch (Exception e) {
+			onSerialIoError(e);
+		}
+	}
 
-    private void receive(ArrayDeque<byte[]> datas) {
-        
+	private void receive(ArrayDeque<byte[]> datas) {
+		
 		//	Media player instance created (for sound support)
 		MediaPlayer mediaPlayer = MediaPlayer.create(getActivity().getApplicationContext(), R.raw.beep);
-        
+		
 		SpannableStringBuilder spn = new SpannableStringBuilder();
-        for (byte[] data : datas) {
-            if (hexEnabled) {
-                spn.append(TextUtil.toHexString(data)).append('\n');
-            } else {
-                String msg = new String(data);
-                if (newline.equals(TextUtil.newline_crlf) && msg.length() > 0) {
-                    // don't show CR as ^M if directly before LF
-                    msg = msg.replace(TextUtil.newline_crlf, TextUtil.newline_lf);
-                    // special handling if CR and LF come in separate fragments
-                    if (pendingNewline && msg.charAt(0) == '\n') {
-                        if(spn.length() >= 2) {
-                            spn.delete(spn.length() - 2, spn.length());
-                        } else {
-                            Editable edt = receiveText.getEditableText();
-                            if (edt != null && edt.length() >= 2)
-                                edt.delete(edt.length() - 2, edt.length());
-                        }
-                    }
-                    pendingNewline = msg.charAt(msg.length() - 1) == '\r';
-                }
-                
+		for (byte[] data : datas) {
+			if (hexEnabled) {
+				spn.append(TextUtil.toHexString(data)).append('\n');
+			} else {
+				String msg = new String(data);
+				if (newline.equals(TextUtil.newline_crlf) && msg.length() > 0) {
+					// don't show CR as ^M if directly before LF
+					msg = msg.replace(TextUtil.newline_crlf, TextUtil.newline_lf);
+					// special handling if CR and LF come in separate fragments
+					if (pendingNewline && msg.charAt(0) == '\n') {
+						if(spn.length() >= 2) {
+							spn.delete(spn.length() - 2, spn.length());
+						} else {
+							Editable edt = receiveText.getEditableText();
+							if (edt != null && edt.length() >= 2)
+								edt.delete(edt.length() - 2, edt.length());
+						}
+					}
+					pendingNewline = msg.charAt(msg.length() - 1) == '\r';
+				}
+				
 				
 				//	NOTE: Bluetooth sometimes sends signals splitted in two
 				//	(example -> hello gets received as h... ello)
@@ -259,66 +259,66 @@ public class TerminalFragment extends Fragment implements ServiceConnection, Ser
 
 				//	Input is considered valid only if there are 3 newlines (standard HIGH case)
 				if(msg.length() > 1 && msg.split("\n").length >= 3) {
-                    try{
-                        // Only for debug purpose (removable)
-                        String values = msg.replaceAll("[^\\d\\n]", "");
-                        int gas = Integer.parseInt(values.split("\n")[0]);
-                        int light = Integer.parseInt(values.split("\n")[1]);
-                        System.out.println(gas);
+					try{
+						// Only for debug purpose (removable)
+						String values = msg.replaceAll("[^\\d\\n]", "");
+						int gas = Integer.parseInt(values.split("\n")[0]);
+						int light = Integer.parseInt(values.split("\n")[1]);
+						System.out.println(gas);
 						System.out.println(light);
 						
 						//	The sound only gets played if "HIGH" state is received
 						String code = msg.split("\n")[2];
 						if(code.contains("HIGH")) {
-                            mediaPlayer.start();
-                        }
-                    }catch(Exception e) {
-                        System.out.println("Invalid input: Not parsing");
-                        System.out.println(e);
-                    }
-                }
-                spn.append(TextUtil.toCaretString(msg, newline.length() != 0));
-            }
-        }
-        receiveText.append(spn);
-    }
+							mediaPlayer.start();
+						}
+					}catch(Exception e) {
+						System.out.println("Invalid input: Not parsing");
+						System.out.println(e);
+					}
+				}
+				spn.append(TextUtil.toCaretString(msg, newline.length() != 0));
+			}
+		}
+		receiveText.append(spn);
+	}
 
-    private void status(String str) {
-        SpannableStringBuilder spn = new SpannableStringBuilder(str + '\n');
-        spn.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorStatusText)), 0, spn.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        receiveText.append(spn);
-    }
+	private void status(String str) {
+		SpannableStringBuilder spn = new SpannableStringBuilder(str + '\n');
+		spn.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorStatusText)), 0, spn.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+		receiveText.append(spn);
+	}
 
-    /*
-     * SerialListener
-     */
-    @Override
-    public void onSerialConnect() {
-        status("connected");
-        connected = Connected.True;
-    }
+	/*
+	 * SerialListener
+	 */
+	@Override
+	public void onSerialConnect() {
+		status("connected");
+		connected = Connected.True;
+	}
 
-    @Override
-    public void onSerialConnectError(Exception e) {
-        status("connection failed: " + e.getMessage());
-        disconnect();
-    }
+	@Override
+	public void onSerialConnectError(Exception e) {
+		status("connection failed: " + e.getMessage());
+		disconnect();
+	}
 
-    @Override
-    public void onSerialRead(byte[] data) {
-        ArrayDeque<byte[]> datas = new ArrayDeque<>();
-        datas.add(data);
-        receive(datas);
-    }
+	@Override
+	public void onSerialRead(byte[] data) {
+		ArrayDeque<byte[]> datas = new ArrayDeque<>();
+		datas.add(data);
+		receive(datas);
+	}
 
-    public void onSerialRead(ArrayDeque<byte[]> datas) {
-        receive(datas);
-    }
+	public void onSerialRead(ArrayDeque<byte[]> datas) {
+		receive(datas);
+	}
 
-    @Override
-    public void onSerialIoError(Exception e) {
-        status("connection lost: " + e.getMessage());
-        disconnect();
-    }
+	@Override
+	public void onSerialIoError(Exception e) {
+		status("connection lost: " + e.getMessage());
+		disconnect();
+	}
 
 }
